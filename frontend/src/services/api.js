9286457e0a938dff6b7d9f7b6d043a87b034bea7
Wouter Altepost <wout.altepost@gmail.com>
2026-04-05 13:21:58 +0200

    Add Railway deployment config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,27 @@
 import axios from 'axios'
+import { useAuthStore } from '@/stores/auth'
 
 const api = axios.create({
-  baseURL: 'http://localhost/api'
+  baseURL: import.meta.env.VITE_API_URL || '/api'
 })
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+  const authStore = useAuthStore()
+  if (authStore.token) {
+    config.headers.Authorization = `Bearer ${authStore.token}`
   }
   return config
 })
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const authStore = useAuthStore()
+      authStore.logout()
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default api
